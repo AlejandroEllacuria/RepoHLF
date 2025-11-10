6cf10cab396ea86779b12e29fe9cb2eb1dcc4a26
AlejandroEllacuria <alexellacuria@yahoo.es>
2025-11-10 09:06:41 +0100

    ultimo

--- funcionesHLF.py
+++ funcionesHLF.py
@@ -1,24 +1,47 @@
 from variablesHLF import *
 import random
 
+'''
+En este .JS voy a diseñar las funciones que necesitaran cada uno de los participantes,
+tanto el ordenador así como el jugador, para poder desarrollar en juego.
+
+'''
+
 #------------------------------------------------------------------------------
+# Definición: Función que creara un tablero de las proporciones solicitas pero modificando la 
+# constante se midifiraría el tamaño del tablero,
+# Parametros: lista que contiene el t ablero a crear.
+# Return: Tablero creado,
 def crearTablero(lstTablero):
     for i in range(TamañoTablero):
         fila = ["*"] * TamañoTablero
         lstTablero.append(fila)
     return lstTablero
 
-
+#------------------------------------------------------------------------------
+# Definición: Función que nos pintara el tablero en pantalla.
+# Parametros: Tablero a pintar en forma de lista
+# Return:
 def pintarTablero(lstTablero):
     for fila in lstTablero:
         print(" ".join(str(c) for c in fila))
     print("----------------------------------------")
 
-
+#------------------------------------------------------------------------------
+# Definición: Función que nos dice si queda algún barco sin hundir.
+# Parametros: Tablero por el que queremos preguntar
+# Return: Booleano que nos indicará si hay objetivos o no.
 def quedanBarcos(lstTablero):
     return any("B" in fila for fila in lstTablero)
 
-
+#------------------------------------------------------------------------------
+# Definición: Función que nos pintara en el tablero el barco con las particularidases indicadas.
+# Parametros: Tablero a pintar en forma de lista.
+#             El nombre que tendra el barco.
+#             El Tamaño del barco.
+#             La cordenada X en el tablero
+#             La cordenada Y en el tablero
+# Return: El tablero resultante tras incluir el nuevo barco.
 def ponerBarco(lstTablero, strNomBarco, intTamano, cordX, cordY):
     if cordY + intTamano > TamañoTablero or cordX >= TamañoTablero:
         print("El barco no cabe en esa posición")
@@ -34,8 +57,17 @@ def ponerBarco(lstTablero, strNomBarco, intTamano, cordX, cordY):
 
     return lstTablero
 
+#------------------------------------------------------------------------------
+# Definición: Función que nos pintara en el tablero el barco con de forma aleatoria.
+# Parametros: Tablero a pintar en forma de lista.
+#             El nombre que tendra el barco.
+#             El Tamaño del barco.
+#             La cordenada X en el tablero
+#             La cordenada Y en el tablero
+# Return: El tablero resultante tras incluir el nuevo barco
 
 def ponerBarcoMac(lstTablero, strNomBarco, intTamano):
+    
     while True:
         cordX = random.randint(0, TamañoTablero - 1)
         cordY = random.randint(0, TamañoTablero - intTamano)
@@ -46,12 +78,21 @@ def ponerBarcoMac(lstTablero, strNomBarco, intTamano):
     return lstTablero
 
 
-
+#------------------------------------------------------------------------------
+# Definición: Función que dispara a un barco y nos indicará si hemos acertado o no, o si hemos fallado,
+#   modificando el tablero según las ditintas posiblidades que se pueden dar.
+# Parametros: Tablero a pintar en forma de lista.
+#             El nombre que tendra el barco.
+#             La cordenada X en el tablero.
+#             La cordenada Y en el tablero.
+#             La cantidad de disparos efectuados.
+# Return: El tablero resultante tras incluir el nuevo barco.
+#         El resultado del disparo.
 def dispararBarco(lstTablero, cordX, cordY, contDisparos):
     print(f"Disparo #{contDisparos} en ({cordX+1}, {cordY+1})")
 
     boolAcierto = True
-    if contDisparos > contDisparosTotales:
+    if contDisparos > ContDisparosTotales:
         print("Juego terminado, has superado el número de disparos.")
         boolAcierto = False
         return lstTablero,boolAcierto 
@@ -80,11 +121,24 @@ def dispararBarco(lstTablero, cordX, cordY, contDisparos):
 
     return lstTablero,boolAcierto
 
+#------------------------------------------------------------------------------
+# Definición: Nos controlará y facilitará cada una de las jugadas efectuadas durante la partida.
+# Parametros: Tablero a pintar en forma de lista.
+#             Contador de disparos.
+#             Condicional que nos indica si el jugador puede seguir jugando.
+#             Quien el el jugador que está jugando
 def jugadaMia(lstTablero, contDisparos, bolSeguirJugando, intQuien):
     bolSeguir = True
+    acierto = False
 
+
+# Mientras la condicion sea cierta seguimos jugando
     while bolSeguir:
+        # Dependiendo quien este jugando intQuien tendra un avlor u otro
+        # 0 Jugador
+        # 1 maquina
         if intQuien == 0:
+            # Se solicitan las cooerdenass aljugador
             print("\nIntroduce coordenadas (1 a", TamañoTablero, ")")
             x = input("Coordenada X: ")
             y = input("Coordenada Y: ")
@@ -93,100 +147,49 @@ def jugadaMia(lstTablero, contDisparos, bolSeguirJugando, intQuien):
                 print("Debes introducir números válidos.")
                 continue
         else:
+            # Se calculan las cooerdenass para la mmaquina
             x = random.randint(1, TamañoTablero)
             y = random.randint(1, TamañoTablero)
 
         x = int(x)
         y = int(y)
-
+        # Sumanos un disparo nuevo
         contDisparos += 1
-        lstTablero, boolPerdido = dispararBarco(lstTablero, x - 1, y - 1, contDisparos)
 
+        lstTablero, acierto = dispararBarco(lstTablero, x - 1, y - 1, contDisparos)
+
+        # Para el caso que ya no queden barcos
         if not quedanBarcos(lstTablero):
             print("¡Has hundido todos los barcos!")
             bolSeguirJugando = False
             bolSeguir = False
+        # Para el caso de que el jugador haya extralimitado el numero de disparos
         elif contDisparos >= 15:
             print("Has agotado tus disparos. Fin del juego.")
             bolSeguirJugando = False
             bolSeguir = False
-        elif boolPerdido == False:
+        # Para el caso que ya no haya acertado el disparo
+        elif not acierto:
             print("Has fallado el tiro.")
-            bolSeguir = boolPerdido
+            bolSeguir = False  
+        # y si no se dan el resto de condiciones 
         else:
+        
             if intQuien == 0:
-                strJugar = input("¿Quieres continuar jugando? (S/N): ")
+                strJugar = input("¡Has acertado! ¿Quieres continuar jugando? (S/N): ")
                 if strJugar.upper() == "S":
-                    bolSeguir = True
+                    bolSeguir = True 
                 else:
                     bolSeguir = False
                     bolSeguirJugando = False
             else:
-                bolSeguir = False  # la máquina solo hace un disparo
-
+                bolSeguir = True  
+                
+        # Pintamos el resultado de la jugada
         print("Tablero Actual")
         print("-------------")
         pintarTablero(lstTablero)
         print("")
 
-    return lstTablero, contDisparos, bolSeguirJugando
-
-'''
-def jugadaMia (lstTablero,contDisparos,bolSeguirJugando,intQuien):
-    
-    bolSeguir = True
-    
-    while bolSeguir:
-        if (intQuien == 0):
-            print("\nIntroduce coordenadas (1 a", TamañoTablero, ")")
-            x = input("Coordenada X: ")
-            y = input("Coordenada Y: ")
-
-            
-            if not (x.isdigit() and y.isdigit()):
-                print("Debes introducir números válidos.")
-                continue
-        else:
-            x = random.randint(1,TamañoTablero)
-            y = random.randint(1, TamañoTablero)
-            
-        x = int(x)
-        y = int(y)
-
-        contDisparos += 1
-        lstTablero , boolPerdido = dispararBarco(lstTablero, x - 1, y - 1,contDisparos,)
-        
-
-        if not quedanBarcos(lstTablero):
-            print("¡Has hundido todos los barcos!")
-            bolSeguirJugando = False
-            bolSeguir = False
-        elif contDisparos >= 15:
-            print("Has agotado tus disparos. Fin del juego.")
-            bolSeguirJugando = False
-            bolSeguir = False
-        elif boolPerdido == False:
-            print("Has Falllado  el tiro.")
-            bolSeguir = boolPerdido
-        else:
-            strJugar = input("¿Quieres continuar jugando? (S/N): ")
-            if strJugar.upper() == "S":
-                bolSeguir = True
-            else:
-                bolSeguir = False
-                bolSeguirJugando = False
-        
-
-       
-        print("Tablero Mio")
-        print("-------------")
-        pintarTablero(lstTablero)
-        print("")
-
-
-    return lstTablero,contDisparos,bolSeguirJugando
-'''
-
-
-
+    return lstTablero, contDisparos, bolSeguirJugando, acierto
 #------------------------------------------------------------------------------
--- variablesHLF.py
+++ variablesHLF.py
@@ -1,11 +1,16 @@
 
+# Constante que indica el tamaño del tablero
 TamañoTablero = 10
+# Contador que indica cuantas oprtunidades de juego tienen cada uno de los oponentes.
 ContDisparosTotales = 15
-
+# Tamaño de los barcos
 intBarco4 = 4
 intBarco3 = 3
 intBarco2 = 2
 intBarco1 = 1
+# Númer total de barcos por jugador
 intNumBarcos = 10
+# Cual es participante que está jugando en cada momento.
 intQuien = 0
+# Si las circustancias del juego permiten seguir el juego o no.
 bolSeguirJugando = True
